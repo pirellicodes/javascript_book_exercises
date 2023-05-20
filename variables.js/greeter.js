4d3e0f1325512af26569249c2462a5291694417e
Pirelli Gray <pirelligray@gmail.com>
2023-05-20 11:45:31 -0600

    Old file updated

--- variables.js/greeter.js
+++
@@ -1,6 +0,0 @@
-//Write a program named greeter.js that greets 'Victor' three times and should use a variable and not hard code the string value
-let person = 'Victor';
-
-console.log(`Good Morning, ${person}.`);
-console.log(`Good Afternoon, ${person}.`);
-console.log(`Good Evening, ${person}.`); 
